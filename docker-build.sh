7c10e7b0e7a7670734a370100749cf240186eecc
Xinyang Yuan <xinyangyuan@hotmail.com>
2019-10-01 21:43:21 +0800

    Test: docker image CI

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,13 @@
 # Build docker images
+echo 'Creating docker images..'
 docker build -t xinyangyuan/mail-nginx ./nginx
 docker build -t xinyangyuan/mail-frontend ./frontend
 docker build -t xinyangyuan/mail-backed ./backend
+echo 'Dokcer images created successfully.'
 
+# Push images to docker hub
+echo 'Push docker images to docker hub..'
+docker push xinyangyuan/mail-nginx
+docker push xinyangyuan/mail-frontend
+docker push xinyangyuan/mail-backed
+echo 'Docker images pushed successfully.'
