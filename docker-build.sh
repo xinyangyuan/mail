f6c8d918bfb3cf1df3cc737f3ea5388e2a77ecdb
Xinyang Yuan <xinyangyuan@hotmail.com>
2019-10-04 20:36:15 +0800

    Backend: open backend port

--- docker-build.sh
+++ docker-build.sh
@@ -5,11 +5,6 @@ docker build -t xinyangyuan/mail-frontend ./frontend
 docker build -t xinyangyuan/mail-backend ./backend
 echo 'Dokcer images created successfully.'
 
-# # Push images to docker hub
-# echo 'Push docker images to docker hub..'
-# docker push xinyangyuan/mail-nginx
-# docker push xinyangyuan/mail-frontend
-# docker push xinyangyuan/mail-backed
-# echo 'Docker images pushed successfully.'
+
 
 
